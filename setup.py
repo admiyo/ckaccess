0889336ca8c8f7579bf81a36ab181d9268379360
Adam Young <ayoung@redhat.com>
2020-07-30 14:26:51 -0400

    barebones project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ckaccess',
+   version='1.0',
+   description='A Web service to check access to other web services',
+   author='Adam Young',
+   author_email='ayoung@redhat.com',
+   packages=['ckaccess'],
+   install_requires=[], 
+)
