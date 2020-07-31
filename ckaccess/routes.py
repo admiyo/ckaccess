6c97cfdb6e173c18728602ae8397ab534e0af97a
Adam Young <ayoung@redhat.com>
2020-07-31 13:21:45 -0400

    MVP

---
+++ ckaccess/routes.py
@@ -0,0 +1,10 @@
+from ckaccess import app
+from flask import request
+import requests 
+
+@app.route('/')
+def test_service():
+    service_url = request.args.get('service_url', '')
+    r = requests.get(service_url) 
+    return 'Testing Service for %s!  for status %d ' % (
+        service_url, r.status_code)
