3aac6c1a3a3c6656d6551f7ce133ac19daa4eb2b
Adam Young <ayoung@redhat.com>
2020-07-31 15:19:30 -0400

    listen on 0.0.0.0

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
 export FLASK_APP=ckaccess
-flask run -p 8080
+flask run -p 8080 --host=0.0.0.0
--- ckaccess/routes.py
+++ ckaccess/routes.py
@@ -1,10 +1,13 @@
 from ckaccess import app
 from flask import request
-import requests 
+import requests
 
 @app.route('/')
 def test_service():
     service_url = request.args.get('service_url', '')
-    r = requests.get(service_url) 
-    return 'Testing Service for %s!  for status %d ' % (
-        service_url, r.status_code)
+    try:
+        r = requests.get(service_url)
+        return 'Testing Service for %s!  for status %d ' % (
+            service_url, r.status_code)
+    except:
+         return 'Could not reach Service %s!' % service_url
